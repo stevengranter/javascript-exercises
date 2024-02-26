9a747819c7ee6346ba3eab8495f156b20c8e9111
teeb <stevengranter@gmail.com>
2024-02-26 11:08:33 -0330

    Complete 01, 02, 03 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function (string, number) {
+    if (number < 0) return 'ERROR'
+    let myString = ''
+    for (let i = 0; i < number; i++) {
+        myString += string
+    }
+    return myString
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function (word) {
+    let charArray = []
+    for (letter in word) {
+        let char = word.at(letter)
+        console.log(char)
+        charArray.unshift(char)
+    }
+    return (charArray.join(''))
+}
 
-};
+reverseString("smile")
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
